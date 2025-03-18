d99a2b5343ef2e98caf0bf22ebc745b5d195fa64
Navneet Prajapati <144175174+NavneetPrajapati31@users.noreply.github.com>
2025-03-19 02:02:41 +0530

    sanity, ui update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -7,75 +9,11 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "Navneet" },
-      _id: 1,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1618901882187-b56b57036cb3?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 2, name: "Navneet" },
-      _id: 2,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1618901882187-b56b57036cb3?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 3, name: "Navneet" },
-      _id: 3,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1618901882187-b56b57036cb3?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 4, name: "Navneet" },
-      _id: 4,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1618901882187-b56b57036cb3?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 5, name: "Navneet" },
-      _id: 5,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1618901882187-b56b57036cb3?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 6, name: "Navneet" },
-      _id: 6,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1618901882187-b56b57036cb3?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+  console.log(JSON.stringify(posts, null, 2));
 
   return (
     <div>
@@ -95,7 +33,7 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupTypeCard, index: number) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
