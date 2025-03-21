8e587b9114e90f247ca0781f83838be9ad555494
Navneet Prajapati <144175174+NavneetPrajapati31@users.noreply.github.com>
2025-03-21 20:54:18 +0530

    search bar update

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
 import { Search } from "lucide-react";
+import Typed from "typed.js";
 
 const SearchForm = ({ query }: { query?: string }) => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const typed = new Typed(searchInputRef.current, {
+      strings: [
+        'Search "AI Startups"',
+        'Search "Health Startups"',
+        'Search "EdTech Startups"',
+      ],
+      typeSpeed: 60,
+      backSpeed: 60,
+      loop: true,
+      attr: "placeholder",
+      smartBackspace: true,
+    });
+
+    return () => typed.destroy();
+  }, []);
+
   return (
     <Form action="/" scroll={false} className="search-form">
       <input
+        ref={searchInputRef}
         name="query"
         defaultValue={query}
         className="search-input"
-        placeholder="Search Startups"
+        placeholder="Search"
       />
+
       <div className="flex gap-2">
         {query && <SearchFormReset />}
         <button type="submit" className="search-btn text-white">
